e289ee0f7b6a4660dc284e88a73394b11d03ac89
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-11-07 14:18:37 +0600

    dynamic urls added in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.golaiv.com',
+  exclude: ['/server-sitemap.xml'],
   generateRobotsTxt: true,
+  robotsTxtOptions: {
+    // policies: [
+    //   {
+    //     userAgent: '*',
+    //     disallow: ['/server-sitemap.xml'],
+    //   },
+    //   {
+    //     userAgent: '*',
+    //     disallow: ['/api'],
+    //   },
+    //   { userAgent: '*', allow: '/' },
+    // ],
+    additionalSitemaps: [
+      `https://www.golaiv.com/sitemap.xml`,
+      `https://www.golaiv.com/server-sitemap.xml`,
+    ],
+  },
 }
