b0f6a40cf7fca9394d7e6abdf19d32d387749ec8
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-11-08 13:54:56 +0600

    added anchor tags in blogs

--- components/LandingPage/Sections/Blogs.jsx
+++ components/LandingPage/Sections/Blogs.jsx
@@ -14,28 +14,30 @@ const Blogs = ({ propData }) => {
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-8 md:mx-14 my-14">
         {propData.map((prop) => (
           <Link key={prop.id} href={`/blogs/${prop['slug']}`}>
-            <div
-              data-aos="zoom-in"
-              data-aos-anchor-placement="top-bottom"
-              className="card card-compact lg:max-width-lg bg-base-100 hover:bg-amber-100 hover:opacity-80 cursor-pointer h-full">
-              <figure>
-                <Image
-                  loader={wpImageLoader}
-                  className="object-cover w-full"
-                  width={826}
-                  height={468}
-                  priority="true"
-                  src={prop['_embedded']['wp:featuredmedia'][0]['source_url']}
-                  alt={prop['_embedded']['wp:featuredmedia'][0]['alt_text']}
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title text-left text-black">{prop['title']['rendered']}</h2>
-                <div
-                  className="text-left text-black"
-                  dangerouslySetInnerHTML={{ __html: prop['excerpt']['rendered'] }}></div>
+            <a>
+              <div
+                data-aos="zoom-in"
+                data-aos-anchor-placement="top-bottom"
+                className="card card-compact lg:max-width-lg bg-base-100 hover:bg-amber-100 hover:opacity-80 cursor-pointer h-full">
+                <figure>
+                  <Image
+                    loader={wpImageLoader}
+                    className="object-cover w-full"
+                    width={826}
+                    height={468}
+                    priority="true"
+                    src={prop['_embedded']['wp:featuredmedia'][0]['source_url']}
+                    alt={prop['_embedded']['wp:featuredmedia'][0]['alt_text']}
+                  />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-left text-black">{prop['title']['rendered']}</h2>
+                  <div
+                    className="text-left text-black"
+                    dangerouslySetInnerHTML={{ __html: prop['excerpt']['rendered'] }}></div>
+                </div>
               </div>
-            </div>
+            </a>
           </Link>
         ))}
       </div>
